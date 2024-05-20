568de675dc715f6cc964f4020e5246e7a671edad
Leandro Marques <leandromarques.n@gmail.com>
2024-05-20 13:11:19 -0300

    feat: adcionar recursos ao component bebidas
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/pages/Main/Bebidas/index.tsx

--- src/pages/Main/Bebidas/index.tsx
+++ src/pages/Main/Bebidas/index.tsx
@@ -1,7 +1,15 @@
 import Head from "../../../components/Head"
+import { SnackTitles } from "../../../components/SnanckTitle/SnackTitle"
 
 export default function Bebidas() {
     return (
-        < Head title="Bebidas" description="bebidas para todos os gostos!"/>
+        (
+            <>
+               
+                < Head title="Bebidas" description="bebidas para todos os gostos!"/>
+                
+                <SnackTitles>Bebidas bem geladinhas</SnackTitles>
+            </>
+        )
     )
 }
