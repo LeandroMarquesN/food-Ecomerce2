970705f3764b6f2cd2e3a1fa7eee7a6cfc63531b
Leandro Marques <leandromarques.n@gmail.com>
2024-05-20 15:22:33 -0300

    feat: adcionar recursos para a pagina do projeto burguers e um novo componente o snacks
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   src/components/Snacks/index.tsx
    	new file:   src/components/Snacks/styles.ts
    	modified:   src/pages/Main/Burguers/index.tsx

--- src/pages/Main/Burguers/index.tsx
+++ src/pages/Main/Burguers/index.tsx
@@ -1,12 +1,37 @@
 import Head from "../../../components/Head"
 import { SnackTitles } from "../../../components/SnanckTitle/SnackTitle"
+import { Snacks } from "../../../components/Snacks"
+
 
 export default function Burguers() {
+
+    const  data= [
+        {
+            id:1,
+            snack:"burguer",
+            name:"Mega",
+            description:
+            'O artesanal tamanho família recheado com três suculentos, queijos e bacon.',
+            price:25.5,
+            image:"https://cdn.pixabay.com/photo/2024/05/10/10/51/ai-generated-8752901_1280.jpg"
+        },
+        {
+            id:2,
+            snack:"burguer",
+            name:"Extra bacon",
+            description:
+            'Criado para os amantes de bacon, possui em todas as suas comadas bacon bem assadoe ainda queijo e carne',
+            price:23.5,
+            image:"https://imgur.com/B4J04AJ.jpg"
+        }
+    ]
+
     return (
         <>
             < Head title="Hamburgueres" description="Os melhores Hamburgueres das galaxias!"/>
 
             <SnackTitles>Burgueres</SnackTitles>
+            <Snacks snacks={data}></Snacks>
         </>
     )
 }
