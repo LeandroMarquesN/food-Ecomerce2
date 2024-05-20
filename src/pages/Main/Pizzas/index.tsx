2343e33d914188bef2cabba1d93ba9f1650b3ee5
Leandro Marques <leandromarques.n@gmail.com>
2024-05-20 13:36:36 -0300

    feat: recursos a pages On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   src/pages/Main/Pizzas/index.tsx
    	modified:   src/pages/Main/Sobremesas/index.tsx

--- src/pages/Main/Pizzas/index.tsx
+++ src/pages/Main/Pizzas/index.tsx
@@ -1,8 +1,11 @@
 import Head from "../../../components/Head"
-
+import { SnackTitles } from "../../../components/SnanckTitle/SnackTitle"
 
 export default function Pizzas() {
     return (
-        < Head title="Pizzas" description="Os mais variados sabores de Pizzas"/>
+        <>
+            < Head title="Pizzas" description="Os mais variados sabores de Pizzas"/>
+            <SnackTitles>Pizzas </SnackTitles>
+        </>
     )
 }
--- src/pages/Main/Sobremesas/index.tsx
+++ src/pages/Main/Sobremesas/index.tsx
@@ -1,8 +1,12 @@
 
 import Head from "../../../components/Head"
+import { SnackTitles } from "../../../components/SnanckTitle/SnackTitle"
 
 export default function Sobremesas() {
     return (
-        < Head title="Sobremesas" description="Sobremesas mais delciosas"/>   
+        <>
+            < Head title="Sobremesas" description="Sobremesas mais delciosas"/> 
+            < SnackTitles>Sobremesas</SnackTitles>
+        </>
     )
 }
