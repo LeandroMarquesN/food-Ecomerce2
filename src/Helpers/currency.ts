30f2550ec6edc5db8c85fba5dec19c5b45668446
Leandro Marques <leandromarques.n@gmail.com>
2024-05-22 13:58:14 -0300

    Feat:adcionar recursos na pasta helper para formatar o preço da moeda corrente
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   src/Helpers/currency.ts
    	modified:   src/components/Snacks/index.tsx

---
+++ src/Helpers/currency.ts
@@ -0,0 +1,8 @@
+
+export default function correncyFormat(currency:number){
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+
+    }).format(currency)
+} 
--- src/components/Snacks/index.tsx
+++ src/components/Snacks/index.tsx
@@ -1,6 +1,7 @@
 
 import {FiPlus} from 'react-icons/fi'
 import { Container } from "./styles";
+import correncyFormat from '../../Helpers/currency';
 
 interface SnacksProps {
     snacks: any[];
@@ -15,7 +16,7 @@ export function Snacks( { snacks }: SnacksProps){
                     <img src={snack.image} alt={snack.name} />
                     <p>{snack.description}</p>
                     <div>
-                        <strong>{snack.price}</strong>
+                        <strong>{correncyFormat(snack.price)}</strong>
                         <button type='button'>
                             <FiPlus />
                         </button>
